e1623772761ef0cc80109910cc83aab6f332511b
Geraldo Domingos <geraldo.dsn2000@gmail.com>
2019-12-21 21:09:23 -0300

    Finalização do Login

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import Logo from './Assets/logo.png'
+import Bicycle from './Assets/bicycle.svg'
+import Wave from './Assets/wave.png'
+
+import './styles/Login.css'
+
+
+const Login = () => {
+    return (
+        <div>
+            <img src={Wave} className='wave' alt='wave'/>
+            <div className='join-container'>
+                <div className='img'>
+                    <img src={Bicycle} alt='design' />
+                </div>
+                <div className='login-container'>
+                    <form>
+                        <img className='chat' src={Logo} alt='logo' />
+                        <h2>Login</h2>
+                        <div className='input-div one focus'>
+                            <div className='username-container'>
+                                <h5>Email</h5>
+                                <input className='input' type='text' />
+                            </div>
+                        </div>
+                        <div className='input-div two focus'>
+                            <div className='comunity-container'>
+                                <h5>Senha</h5>
+                                <input className='input' type='password'/>
+                            </div>
+                        </div>
+                        <div className='checkbox'>
+                            <input type='checkbox'/> <span>Manter-se Logado</span>
+                        </div>
+                        <Link style={{ textDecoration: 'none' }}>
+                            <button type='submit' className='btn'>LOGIN</button>
+                        </Link>
+                        <div className='cadastre'>
+                            <span>Não possui cadastro? </span><Link to='/cadastro' className='link' style={{ textDecoration: 'none', color:'#32be32' }} >Clique Aqui!</Link>
+                        </div>
+                    </form>
+                </div>
+            </div >
+        </div>
+    )
+}
+
+export default Login
