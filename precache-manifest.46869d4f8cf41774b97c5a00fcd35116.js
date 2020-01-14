8bd0f77c7efe42235de4616dbfa41b9b785011f1
Geraldo Domingos <geraldo.dsn2000@gmail.com>
2020-01-14 13:27:29 -0300

    Updates

---
+++ precache-manifest.46869d4f8cf41774b97c5a00fcd35116.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cb6c33cbce140e30de39b834a4efe8a",
+    "url": "/bike-premium/index.html"
+  },
+  {
+    "revision": "fd1721fd67ac131d301f",
+    "url": "/bike-premium/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "03b2bbc978479903ed43",
+    "url": "/bike-premium/static/css/main.6764aacf.chunk.css"
+  },
+  {
+    "revision": "fd1721fd67ac131d301f",
+    "url": "/bike-premium/static/js/2.7c04571e.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/bike-premium/static/js/2.7c04571e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "03b2bbc978479903ed43",
+    "url": "/bike-premium/static/js/main.abca6289.chunk.js"
+  },
+  {
+    "revision": "3480584e73db12c4b442",
+    "url": "/bike-premium/static/js/runtime-main.34179f7a.js"
+  },
+  {
+    "revision": "ecefa4a8e5f9e908258525cc2aae1f7e",
+    "url": "/bike-premium/static/media/Banner.ecefa4a8.png"
+  },
+  {
+    "revision": "f439cf2d944e676aa70609163054eacd",
+    "url": "/bike-premium/static/media/BannerLargo.f439cf2d.png"
+  },
+  {
+    "revision": "537bf1d350ba9d9c0e90a2b135976a86",
+    "url": "/bike-premium/static/media/BannerPequeno.537bf1d3.png"
+  },
+  {
+    "revision": "ed9811d8676d8ce86ca6ca3143c88975",
+    "url": "/bike-premium/static/media/Ciclista.ed9811d8.png"
+  },
+  {
+    "revision": "1650132350f15261cb195459fd1b0136",
+    "url": "/bike-premium/static/media/Road_Rage.16501323.otf"
+  },
+  {
+    "revision": "7faa6da0948cd53bdee0e85b941dcb4c",
+    "url": "/bike-premium/static/media/asfalto.7faa6da0.png"
+  },
+  {
+    "revision": "a92c47824d82d620deb8e30b7cff9361",
+    "url": "/bike-premium/static/media/banner.a92c4782.png"
+  },
+  {
+    "revision": "0fd8b355c15e9bb2090f5e8688fe52a2",
+    "url": "/bike-premium/static/media/bicycle.0fd8b355.svg"
+  },
+  {
+    "revision": "286c8c03eb39daacedb2f39fc13a203a",
+    "url": "/bike-premium/static/media/bike.286c8c03.jpg"
+  },
+  {
+    "revision": "3d43264e79f8e2e3650ba06e5e51430c",
+    "url": "/bike-premium/static/media/bmx.3d43264e.png"
+  },
+  {
+    "revision": "2b6bc9b21a8b5cdedb8216709f76b764",
+    "url": "/bike-premium/static/media/logo.2b6bc9b2.png"
+  },
+  {
+    "revision": "b5858f96b7d2658f8d7573f90c9f3e5d",
+    "url": "/bike-premium/static/media/passeio.b5858f96.png"
+  },
+  {
+    "revision": "d495970fa1f8950355b9abf491647ade",
+    "url": "/bike-premium/static/media/propaganda.d495970f.png"
+  },
+  {
+    "revision": "3de509c94f0d4b1d5254ce1b98136b5a",
+    "url": "/bike-premium/static/media/trilhas.3de509c9.png"
+  },
+  {
+    "revision": "f394b483d4a59f89359a4caedce45292",
+    "url": "/bike-premium/static/media/user.f394b483.png"
+  },
+  {
+    "revision": "6d252a00c562aee3ae3bab8ed4658f52",
+    "url": "/bike-premium/static/media/wave.6d252a00.png"
+  }
+]);
